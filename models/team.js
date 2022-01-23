63d88dbbfc6f969afdaf2c64f9c0628b3613dc5b
hayatulemon <hayatulemon340@gmail.com>
2022-01-24 00:56:43 +0600

    nothing

--- models/team.js
+++ models/team.js
@@ -21,7 +21,6 @@ router.post("/", async (req, res) => {
 
     try {
         const result = await newTeam.save();
-        console.log(result);
         res.status(200).json(result);
     } catch (err) {
         res.status(500).json(err);
